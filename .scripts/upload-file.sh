bed84bd135d0ceacb5347ee325e3ea0d9febe3f9
miele-nl <mielenl73@gmail.com>
2021-11-13 23:15:47 +0100

    separate local and remote dir

--- .scripts/upload-file.sh
+++ .scripts/upload-file.sh
@@ -3,7 +3,9 @@
 ftpHost=$1
 ftpUser=$2
 ftpPassword=$3
-file=$4
+localDir=$4
+remoteDir=$5
+file=$6
 
 if [[  -z $1 || -z $2 || -z $3 || -z $4 ]]; then
     echo "required arguments missing"
@@ -14,6 +16,8 @@ ftp -n $ftpHost <<END_SCRIPT
 quote USER $ftpUser
 quote PASS $ftpPassword
 binary
+lcd $localDir
+cd $remoteDir
 put $file
 quit
 END_SCRIPT
