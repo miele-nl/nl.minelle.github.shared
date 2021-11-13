e22cfc5ac0ea4b0d0404d03561f031d7f6ce7d98
miele-nl <mielenl73@gmail.com>
2021-11-13 21:26:56 +0100

    without continuation characters

--- .scripts/prepare-build.sh
+++ .scripts/prepare-build.sh
@@ -13,7 +13,7 @@ for var in "${vars[@]}"; do
   key=$(echo $var | jq -r '.key' | tr [:lower:] [:upper:])
   value=$(echo $var | jq -r '.value')
 
-  stmt="--dart-define ${key//-/_}=\"$value\" \\"
+  stmt="--dart-define ${key//-/_}=\"$value\""
   echo "new statement: $stmt"
 
   result=$(echo "$result $stmt")
