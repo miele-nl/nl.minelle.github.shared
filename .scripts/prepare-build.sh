d86ade5cafa9391e0596e655c87db3c936ca6170
miele-nl <mielenl73@gmail.com>
2021-11-13 19:58:53 +0100

    add build parameters

---
+++ .scripts/prepare-build.sh
@@ -0,0 +1,24 @@
+variables=$1
+
+if [[ -z $1 ]]; then
+  variables="[]"
+fi
+
+# read each item in the JSON array to an item in the Bash array
+readarray -t vars < <(echo $variables | jq -c '.[]')
+
+# iterate through the Bash array
+result=""
+for var in "${vars[@]}"; do
+  key=$(echo $var | jq -r '.key' | tr [:lower:] [:upper:])
+  value=$(echo $var | jq -r '.value')
+
+  stmt="--dart-define ${key//-/_}=\"$value\" \\"
+  echo "new statement: $stmt"
+
+  result=$("$result
+  $stmt")
+
+done
+
+echo $result > _prepare_build.txt
