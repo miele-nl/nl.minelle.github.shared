90e79ae67558ad6f472c9bc4bb87609f3a942936
miele-nl <mielenl73@gmail.com>
2021-11-13 23:36:14 +0100

    remove localdir parameter

--- .scripts/upload-file.sh
+++ .scripts/upload-file.sh
@@ -3,11 +3,10 @@
 ftpHost=$1
 ftpUser=$2
 ftpPassword=$3
-localDir=$4
-remoteDir=$5
-file=$6
+remoteDir=$4
+file=$5
 
-if [[  -z $1 || -z $2 || -z $3 || -z $4 || -z $5 || -z $6 ]]; then
+if [[  -z $1 || -z $2 || -z $3 || -z $4 || -z $5 ]]; then
     echo "required arguments missing"
     exit 1
 fi
@@ -16,7 +15,6 @@ ftp -n $ftpHost <<END_SCRIPT
 quote USER $ftpUser
 quote PASS $ftpPassword
 binary
-lcd $localDir
 cd $remoteDir
 put $file
 quit
